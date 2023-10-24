1d58710f4276d572353cfea84652ad8c6ba96f02
SevKohler <kohlerseverin@posteo.de>
2023-10-24 12:57:25 +0200

    added basic spring structure

---
+++ src/test/java/com/example/aft/AftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
