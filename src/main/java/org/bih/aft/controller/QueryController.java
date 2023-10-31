be81bb9449b1b2bad04d9a5fcb8d6a5b6bcd0f7e
SevKohler <kohlerseverin@posteo.de>
2023-10-31 17:37:37 +0100

    added test compose

--- src/main/java/org/bih/aft/controller/QueryController.java
+++ src/main/java/org/bih/aft/controller/QueryController.java
@@ -6,6 +6,8 @@ import org.bih.aft.Exceptions.InvalidCountQuery;
 import org.bih.aft.controller.dao.AQLinput;
 import org.bih.aft.service.QueryService;
 import org.ehrbase.openehr.sdk.client.openehrclient.OpenEhrClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +16,44 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/query")
 public class QueryController {
     private final OpenEhrClient openEhrClient;
+    private static final Logger LOG = LoggerFactory.getLogger(QueryService.class);
 
     public QueryController(OpenEhrClient openEhrClient) {
         this.openEhrClient = openEhrClient;
     }
     @PostMapping(
+            path = "/federate",
             consumes = "application/json",
             produces = "application/json")
-    public ResponseEntity<Object> query(@RequestBody String json) {
+    public ResponseEntity<Object> federateQuery(@RequestBody String json) {
+        LOG.info("Query received");
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             AQLinput aQlQuery = objectMapper.readValue(json, AQLinput.class);
-            return new ResponseEntity<>(new QueryService(openEhrClient).query(aQlQuery), HttpStatus.OK);
+            return new ResponseEntity<>(new QueryService(openEhrClient).federateQuery(aQlQuery), HttpStatus.OK);
         } catch (JsonProcessingException e) {
             return new ResponseEntity<>("{ \"message\" : \"Json malformed\" }", HttpStatus.BAD_REQUEST);
         }catch (InvalidCountQuery invalidCountException){
             return new ResponseEntity<>("{ \"message\" : "+invalidCountException.getMessage()+" }", HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PostMapping(
+            path = "/local",
+            consumes = "application/json",
+            produces = "application/json")
+    public ResponseEntity<Object> localQuery(@RequestBody String json) {
+        LOG.info("Query received");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            AQLinput aQlQuery = objectMapper.readValue(json, AQLinput.class);
+            return new ResponseEntity<>(new QueryService(openEhrClient).localQuery(aQlQuery), HttpStatus.OK);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("{ \"message\" : \"Json malformed\" }", HttpStatus.BAD_REQUEST);
+        }catch (InvalidCountQuery invalidCountException){
+            return new ResponseEntity<>("{ \"message\" : "+invalidCountException.getMessage()+" }", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
--- src/main/java/org/bih/aft/service/QueryService.java
+++ src/main/java/org/bih/aft/service/QueryService.java
@@ -1,5 +1,7 @@
 package org.bih.aft.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bih.aft.Exceptions.InvalidCountQuery;
 import org.bih.aft.controller.dao.AQLinput;
 import org.bih.aft.service.dao.FeasabilityOutput;
@@ -11,32 +13,87 @@ import org.ehrbase.openehr.sdk.client.openehrclient.OpenEhrClient;
 import org.ehrbase.openehr.sdk.generator.commons.aql.query.NativeQuery;
 import org.ehrbase.openehr.sdk.generator.commons.aql.query.Query;
 import org.ehrbase.openehr.sdk.generator.commons.aql.record.Record1;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class QueryService {
     private final OpenEhrClient openEhrClient;
+    private static final Logger LOG = LoggerFactory.getLogger(QueryService.class);
 
     public QueryService(OpenEhrClient openEhrClient) {
         this.openEhrClient = openEhrClient;
     }
 
-    public FeasabilityOutput query(AQLinput aqlQuery) throws InvalidCountQuery {
+    public List<FeasabilityOutput> federateQuery(AQLinput aqlQuery) throws InvalidCountQuery {
         validateQueryForCount(aqlQuery);
+        LOG.info("Query validated");
+        FeasabilityOutput feasabilityOutput = federate(aqlQuery);
+        return generateFeasabilityOutput(feasabilityOutput, executeAqlQuery(aqlQuery.getAql()));
+    }
+
+    public List<FeasabilityOutput> localQuery(AQLinput aqlQuery) throws InvalidCountQuery {
+        validateQueryForCount(aqlQuery);
+        LOG.info("Query validated");
         return generateFeasabilityOutput(executeAqlQuery(aqlQuery.getAql()));
     }
 
-    private FeasabilityOutput generateFeasabilityOutput(List<Record1<String>> aqlResult) {
+    private FeasabilityOutput federate(AQLinput aqlQuery) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject aql = new JSONObject();
+        aql.put("aql", aqlQuery.getAql());
+        final String uri = "http://localhost:8091/query/local";
+        HttpEntity request =
+                new HttpEntity(aql.toString(), headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.postForObject(uri, request, String.class);
+        LOG.info("Query federated");
         FeasabilityOutput feasabilityOutput = new FeasabilityOutput();
-        if(Integer.parseInt(aqlResult.get(0).value1())>10){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            feasabilityOutput = objectMapper.readValue(result, FeasabilityOutput.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return feasabilityOutput;
+    }
+
+
+    private List<FeasabilityOutput> generateFeasabilityOutput(FeasabilityOutput feasabilityOutputFederated, List<Record1<String>> aqlResult) {
+        LOG.info("Query executed");
+        FeasabilityOutput feasabilityOutput = new FeasabilityOutput();
+        if (Integer.parseInt(aqlResult.get(0).value1()) > 10) {
             feasabilityOutput.setLocation("Charite");
             feasabilityOutput.setPatients(aqlResult.get(0).value1());
-        }else{
+        } else {
             feasabilityOutput.setLocation("Charite");
             feasabilityOutput.setPatients("NA");
         }
-        return feasabilityOutput;
+        List<FeasabilityOutput> federatedResults = new ArrayList<>();
+        federatedResults.add(feasabilityOutputFederated);
+        federatedResults.add(feasabilityOutput);
+        LOG.info("Query finalized");
+        return federatedResults;
+    }
+
+    private List<FeasabilityOutput> generateFeasabilityOutput(List<Record1<String>> aqlResult) {
+        LOG.info("Query executed");
+        FeasabilityOutput feasabilityOutput = new FeasabilityOutput();
+        if (Integer.parseInt(aqlResult.get(0).value1()) > 10) {
+            feasabilityOutput.setLocation("Charite");
+            feasabilityOutput.setPatients(aqlResult.get(0).value1());
+        } else {
+            feasabilityOutput.setLocation("Charite");
+            feasabilityOutput.setPatients("NA");
+        }
+        LOG.info("Query finalized");
+        return List.of(feasabilityOutput);
     }
 
     private void validateQueryForCount(AQLinput aqlQuery) {
@@ -50,20 +107,20 @@ public class QueryService {
     }
 
     private void checkCount(SelectClause selectClause) {
-        List<SelectExpression> selectExpressions= selectClause.getStatement();
-        try{
+        List<SelectExpression> selectExpressions = selectClause.getStatement();
+        try {
             AggregateFunction aggregateFunction = (AggregateFunction) selectExpressions.get(0).getColumnExpression();
-            if(!aggregateFunction.getFunctionName().equals(AggregateFunction.AggregateFunctionName.COUNT)) {
+            if (!aggregateFunction.getFunctionName().equals(AggregateFunction.AggregateFunctionName.COUNT)) {
                 throw new InvalidCountQuery("Function has to be COUNT");
             }
-        }catch (ClassCastException e){
+        } catch (ClassCastException e) {
             throw new InvalidCountQuery("No COUNT included in Select statement");
         }
     }
 
     private void checkSize(SelectClause selectClause) {
-        List<SelectExpression> selectExpressions= selectClause.getStatement();
-        if(selectExpressions.size()>1) {
+        List<SelectExpression> selectExpressions = selectClause.getStatement();
+        if (selectExpressions.size() > 1) {
             throw new InvalidCountQuery("Only one Select clause with one statement is allowed");
         }
     }
